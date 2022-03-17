d722c03a40fb750161f349d7c05be2c96370a061
Denis Dolbilin <kombogame6@gmail.com>
2022-03-18 01:39:10 +0200

    feat: almost finished with ui part of a task

--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -1,8 +1,64 @@
 import { useState } from 'react';
-import { CarsharingComponent } from '../../components';
+import { CarsharingComponent, HamburgerMenu } from '../../components';
+import { CarsharingSliderComponent } from '../../components/carsharing-slider-component';
+import styles from './main-page.module.css';
+import { images } from '../../shared/images';
 
 export function MainPage() {
   const [isClicked, setClicked] = useState(false);
+  const [isMenuActive, setMenuActive] = useState(true);
 
-  return <CarsharingComponent isClicked={isClicked} setClicked={setClicked} />;
+  const getWidth = () => window.innerWidth;
+
+  const [animation, setAnimation] = useState({
+    translate: 0,
+    transition: 0.45,
+    activeIndex: 0,
+  });
+
+  const { translate, transition, activeIndex } = animation;
+
+  const nextSlide = () => {
+    if (activeIndex === images.length - 1) {
+      return setAnimation({
+        ...animation,
+        activeIndex: 0,
+      });
+    }
+
+    setAnimation({
+      ...animation,
+      activeIndex: activeIndex + 1,
+    });
+  };
+
+  const prevSlide = () => {
+    if (activeIndex === 0) {
+      return setAnimation({
+        ...animation,
+        activeIndex: images.length - 1,
+      });
+    }
+
+    setAnimation({
+      ...animation,
+      activeIndex: activeIndex - 1,
+    });
+  };
+
+  return (
+    <div className={styles.container}>
+      <HamburgerMenu isMenuActive={isMenuActive} />
+      <CarsharingComponent isClicked={isClicked} setClicked={setClicked} />
+      <CarsharingSliderComponent
+        translate={translate}
+        transition={transition}
+        width={getWidth()}
+        sliderImagesArr={images}
+        prevSlide={prevSlide}
+        nextSlide={nextSlide}
+        activeIndex={activeIndex}
+      />
+    </div>
+  );
 }
