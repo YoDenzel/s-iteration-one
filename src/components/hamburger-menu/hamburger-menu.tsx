3416dd5cf6a4ccd31dbd2545bac08872bdc337e7
Denis Dolbilin <kombogame6@gmail.com>
2022-03-21 01:18:36 +0200

    feat: responsive and adaptive ui, hamburger menu finished

--- src/components/hamburger-menu/hamburger-menu.tsx
+++ src/components/hamburger-menu/hamburger-menu.tsx
@@ -1,18 +1,47 @@
+import { Icons } from '../../shared/icons';
+import { TMenuIcons, TMenuTitlesArr } from '../../shared/types';
+import { ChangeLanguageButton } from '../change-language-button';
+import { MenuComponent } from '../menu-component';
 import styles from './hamburger-menu.module.css';
 
 type THamburgerMenu = {
   isMenuActive: boolean;
-  setMenuActive?: (v: boolean) => void;
+  setMenuActive: (v: boolean) => void;
+  language: string;
+  setLanguage: (v: string) => void;
+  menuIconsArr: TMenuIcons[];
+  menuTitlesArr: TMenuTitlesArr[];
+  windowWidth: number;
 };
 
-export function HamburgerMenu({ isMenuActive }: THamburgerMenu) {
-  return isMenuActive ? (
+export function HamburgerMenu({
+  isMenuActive,
+  language,
+  setLanguage,
+  setMenuActive,
+  menuIconsArr,
+  menuTitlesArr,
+  windowWidth,
+}: THamburgerMenu) {
+  return !isMenuActive ? (
     <aside className={styles.container}>
-      <div className={styles.burger_btn}>
-        <span className={styles.burger_btn_middle} />
-      </div>
+      <button className={styles.burger_btn} onClick={() => setMenuActive(true)}>
+        <Icons.OpenHamburgerMenuIcon
+          color={windowWidth < 768 ? '#000' : '#fff'}
+        />
+      </button>
+      {windowWidth > 765 && (
+        <ChangeLanguageButton language={language} setLanguage={setLanguage} />
+      )}
     </aside>
   ) : (
-    <div>Hello World</div>
+    <MenuComponent
+      windowWidth={windowWidth}
+      menuIconsArr={menuIconsArr}
+      menuTitlesArr={menuTitlesArr}
+      setMenuActive={setMenuActive}
+      language={language}
+      setLanguage={setLanguage}
+    />
   );
 }
