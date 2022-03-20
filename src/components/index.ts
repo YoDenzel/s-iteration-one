cec061b14c38782accc14f5351325845c37fc75d
Denis Dolbilin <kombogame6@gmail.com>
2022-03-21 01:18:55 +0200

    feat: new component exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,8 @@
 export { CarsharingComponent } from './carsharing-component';
 export { HamburgerMenu } from './hamburger-menu';
+export { SliderPaginationDots } from './slider-pagination-dots';
+export { SingleSlideComponent } from './single-slide-component';
+export { CarsharingSliderComponent } from './carsharing-slider-component';
+export { CarsharingSliderArrows } from './carsharing-slider-arrows';
+export { MenuComponent } from './menu-component';
+export { ChangeLanguageButton } from './change-language-button';
