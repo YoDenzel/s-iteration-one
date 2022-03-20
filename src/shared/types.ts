e9e3c48235a1d0658f501ee8011ca549d00893ae
Denis Dolbilin <kombogame6@gmail.com>
2022-03-21 01:09:14 +0200

    feat: types for menu arrays

--- src/shared/types.ts
+++ src/shared/types.ts
@@ -1,5 +1,35 @@
+import { SetStateAction, Dispatch } from 'react';
+
 export type TIconProps = {
   height?: number;
   width?: number;
   color?: string;
+  isActive?: boolean
+};
+
+export type TSliderImagesArr = {
+  title: string;
+  description: string;
+  buttonColor: string;
+  sliderImage: any;
+};
+
+export type TSetAnimation = Dispatch<
+  SetStateAction<{ translate: number; transition: number; activeIndex: number }>
+>;
+
+export type TAnimation = {
+  translate: number;
+  transition: number;
+  activeIndex: number;
+};
+
+export type TMenuTitlesArr = {
+  title: string;
+  linkTo: string;
+};
+
+export type TMenuIcons = {
+  icon: ({ height, width }: TIconProps) => JSX.Element;
+  linkTo: string;
 };
