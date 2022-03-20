d5fe1b415ee678cb55d9fbcc6822bd3ca255ae95
Denis Dolbilin <kombogame6@gmail.com>
2022-03-21 01:12:10 +0200

    feat: locally used fonts

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { App } from './app/App';
 import { BrowserRouter } from 'react-router-dom';
+import './fonts/Roboto/Roboto-Black.ttf';
+import './fonts/Roboto/Roboto-Bold.ttf';
+import './fonts/Roboto/Roboto-Light.ttf';
+import './fonts/Roboto/Roboto-Medium.ttf';
+import './fonts/Roboto/Roboto-Regular.ttf';
 
 ReactDOM.render(
   <React.StrictMode>
