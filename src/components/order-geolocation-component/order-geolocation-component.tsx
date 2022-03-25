6bd98d43b5e693eb0f771a0de528772dccacf3cd
Denis Dolbilin <kombogame6@gmail.com>
2022-03-25 15:26:10 +0200

    feat: added props for input

--- src/components/order-geolocation-component/order-geolocation-component.tsx
+++ src/components/order-geolocation-component/order-geolocation-component.tsx
@@ -2,41 +2,49 @@ import { useState } from 'react';
 import { TextInput } from '../text-input';
 import styles from './order-geolocation-component.module.scss';
 
+const addressesArr = [
+  {
+    city: 'Ульяновск',
+    address: [
+      'Хрустальная 42',
+      'Нариманова 42',
+      'Варейкиса 36',
+      'Кольцевая 49',
+    ],
+  },
+  {
+    city: 'Самара',
+    address: [
+      'Гагарина 109',
+      'Мориса Тореза 137',
+      'Гагарина 296',
+      'Авроры 103',
+    ],
+  },
+  {
+    city: 'Казань',
+    address: ['Яхина 8', 'Шахиди 75', 'Нариманова 15', 'Профсоюзная 48'],
+  },
+  {
+    city: 'Тольятти',
+    address: [
+      'Горького 55',
+      'Первомайская 113',
+      'Льва Толстого 20',
+      'Студенческий 90',
+    ],
+  },
+];
+
 export function OrderGeolocationComponent() {
   const [input, setInput] = useState('');
-  const addressesArr = [
-    {
-      city: 'Ульяновск',
-      address: [
-        'Хрустальная 42',
-        'Нариманова 42',
-        'Варейкиса 36',
-        'Кольцевая 49',
-      ],
-    },
-    {
-      city: 'Самара',
-      address: [
-        'Гагарина 109',
-        'Мориса Тореза 137',
-        'Гагарина 296',
-        'Авроры 103',
-      ],
-    },
-    {
-      city: 'Казань',
-      address: ['Яхина 8', 'Шахиди 75', 'Нариманова 15', 'Профсоюзная 48'],
-    },
-    {
-      city: 'Тольятти',
-      address: [
-        'Горького 55',
-        'Первомайская 113',
-        'Льва Толстого 20',
-        'Студенческий 90',
-      ],
-    },
-  ];
+  const citiesArr: string[] | undefined = [];
+
+  addressesArr.forEach(item => citiesArr.push(item.city));
+
+  console.log(
+    citiesArr.filter(item => item.toLowerCase().includes(input.toLowerCase())),
+  );
 
   return (
     <section>
@@ -46,6 +54,10 @@ export function OrderGeolocationComponent() {
           placeholder="Введите город"
           inputValue={input}
           setInputValue={setInput}
+          listItems={citiesArr.filter(item => {
+            if (item.toUpperCase().includes(input.toUpperCase())) return true;
+            else return false;
+          })}
         />
       </form>
     </section>
