bf6996cdb3d7ff650b0f0de7a02f9249d6d0765f
Denis Dolbilin <kombogame6@gmail.com>
2022-03-30 01:02:57 +0200

    refactor: moved breadcrumbs array

--- src/pages/order-page/order-page.tsx
+++ src/pages/order-page/order-page.tsx
@@ -4,29 +4,11 @@ import {
   HeaderComponent,
   OrderGeolocationComponent,
 } from '../../components';
+import { breadcrumbsArr } from '../../shared/constants';
 import styles from './order-page.module.scss';
 
 export function OrderPage() {
   const [activeComponentIndex, setActiveComponentIndex] = useState(0);
-
-  const breadcrumbsArr = [
-    {
-      title: 'Местоположение',
-      linkTo: '/simbirsoft-iteration-one/order',
-    },
-    {
-      title: 'Модель',
-      linkTo: '/simbirsoft-iteration-one/order',
-    },
-    {
-      title: 'Дополнительно',
-      linkTo: '/simbirsoft-iteration-one/order',
-    },
-    {
-      title: 'Итого',
-      linkTo: '/simbirsoft-iteration-one/order',
-    },
-  ];
 
   return (
     <section className={styles.order_container}>
