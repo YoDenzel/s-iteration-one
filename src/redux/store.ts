04594f377f0195113f161ae6064473354e5c97f0
Denis Dolbilin <kombogame6@gmail.com>
2022-04-01 13:57:41 +0200

    feat: step one reducer

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import mapPointsReducer from './map-points-slice/map-points-slice';
+import stepOneOrderFormReducer from './step-one-order-form/step-one-order-form';
 
 export const store = configureStore({
   reducer: {
     mapPoints: mapPointsReducer,
+    stepOneOrderForm: stepOneOrderFormReducer,
   },
 });
 
