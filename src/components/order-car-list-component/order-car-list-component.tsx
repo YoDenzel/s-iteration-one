eb3f08b4192aed0400a1d838948f1914608017ea
Denis Dolbilin <kombogame6@gmail.com>
2022-04-02 14:28:51 +0200

    feat: added correct wrapepr and stylings

--- src/components/order-car-list-component/order-car-list-component.tsx
+++ src/components/order-car-list-component/order-car-list-component.tsx
@@ -1,3 +1,5 @@
+import styles from './order-car-list-component.module.scss';
+
 export function OrderCarListComponent() {
-  return <div>Hello World</div>;
+  return <div className={styles.container}>Hello World</div>;
 }
