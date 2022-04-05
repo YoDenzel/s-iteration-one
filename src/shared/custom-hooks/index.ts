57510318d96568664de5f6cb0027b4021488357c
Denis Dolbilin <kombogame6@gmail.com>
2022-04-05 02:04:01 +0200

    feat: export for use pagination hook

--- src/shared/custom-hooks/index.ts
+++ src/shared/custom-hooks/index.ts
@@ -1,3 +1,4 @@
 export { useWindowWidth } from './use-window-width';
 export { useAppDispatch, useAppSelector } from './custom-redux-hooks';
 export { useGetData } from './use-get-data';
+export { usePagination } from './use-pagination';
