c0f9ce205e37843040dfde12357c655b0d0abef9
Denis Dolbilin <kombogame6@gmail.com>
2022-04-05 02:05:13 +0200

    feat: logic for pagination

--- src/components/order-car-list-component/order-car-list-component.tsx
+++ src/components/order-car-list-component/order-car-list-component.tsx
@@ -1,30 +1,39 @@
 import { useState } from 'react';
-import { useGetData } from '../../shared/custom-hooks';
-import { createPaginationArray } from '../../shared/functions';
+import { useGetData, usePagination } from '../../shared/custom-hooks';
 import { TCars } from '../../shared/types';
 import { CarsListComponent } from '../cars-list-component';
 import { FilterCarsForm } from '../filter-cars-form';
-import { Pagination } from '../pagination';
 import { PAGE_LIMIT } from './constants';
 import styles from './order-car-list-component.module.scss';
 
 export function OrderCarListComponent() {
   const [activeButtonName, setActiveButtonName] = useState('all');
-  const [page, setPage] = useState(1);
+  const [currentPage, setCurrentPage] = useState(1);
   const { data } = useGetData<TCars>({
     QUERY_KEY: 'cars',
     url: `car?page=1&limit=${PAGE_LIMIT}`,
   });
-  const totalPageCount = Math.ceil((data?.count ?? 83) / PAGE_LIMIT);
-  console.log(createPaginationArray({ start: 1, end: totalPageCount }));
+  const { paginationRange, totalPageCount } = usePagination({
+    currentPage: currentPage,
+    pageSize: PAGE_LIMIT,
+    totalCount: data?.count ?? 1,
+    siblingCount: 1,
+  });
+
   return (
     <div className={styles.container}>
       <FilterCarsForm
         activeButtonName={activeButtonName}
         setActiveButtonName={setActiveButtonName}
       />
-      <CarsListComponent data={data} />
-      <Pagination />
+      <CarsListComponent
+        data={data}
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+        nextPageClickhandler={() => setCurrentPage(prevValue => prevValue + 1)}
+        paginationRange={paginationRange}
+        prevPageClickhandler={() => setCurrentPage(prevValue => prevValue - 1)}
+      />
     </div>
   );
 }
