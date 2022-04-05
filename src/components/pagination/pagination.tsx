829ffbca7f790e7fe93476d810d130f83d8ab032
Denis Dolbilin <kombogame6@gmail.com>
2022-04-05 02:00:57 +0200

    feat: deleted all the logic from here and moved it up

--- src/components/pagination/pagination.tsx
+++ src/components/pagination/pagination.tsx
@@ -1,24 +1,56 @@
-import { useState } from 'react';
-import { usePagination } from '../../shared/custom-hooks/use-pagination';
+import { Icons } from '../../shared/icons';
+import styles from './pagination.module.scss';
 
-export function Pagination() {
-  const [currentPage] = useState(7);
-  const paginationRange = usePagination({
-    currentPage: currentPage,
-    pageSize: 6,
-    totalCount: 84,
-    siblingCount: 1,
-  });
+type TPagination = {
+  currentPage: number;
+  setCurrentPage: (v: number) => void;
+  paginationRange: (string | number)[] | undefined;
+  nextPageClickhandler: () => void;
+  prevPageClickhandler: () => void;
+};
 
+export function Pagination({
+  currentPage,
+  nextPageClickhandler,
+  paginationRange,
+  prevPageClickhandler,
+  setCurrentPage,
+}: TPagination) {
   return (
-    <nav>
+    <nav className={styles.nav_container}>
+      <button
+        className={styles.pagination_item}
+        onClick={() => prevPageClickhandler()}
+      >
+        <Icons.LeftArrow color="#000" />
+      </button>
       {paginationRange?.map((item, index) => {
         if (item === 'DOTS') {
-          return <p key={item + index}>&#8230;</p>;
+          return (
+            <span className={styles.pagination_item_dots} key={item + index}>
+              &#8230;
+            </span>
+          );
         }
 
-        return <p key={Number(item) + index}>{item}</p>;
+        return (
+          <button
+            className={`${styles.pagination_item} ${
+              currentPage === Number(item) && styles.active_page
+            }`}
+            key={Number(item) + index}
+            onClick={() => setCurrentPage(Number(item))}
+          >
+            {item}
+          </button>
+        );
       })}
+      <button
+        className={styles.pagination_item}
+        onClick={() => nextPageClickhandler()}
+      >
+        <Icons.RightArrow color="#000" />
+      </button>
     </nav>
   );
 }
