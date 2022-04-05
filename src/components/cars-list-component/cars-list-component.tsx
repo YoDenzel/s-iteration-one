436fb8ad5cd76ba758b0cc377881ad0fa4491005
Denis Dolbilin <kombogame6@gmail.com>
2022-04-05 02:00:22 +0200

    feat: added pagination component, props for it, also new wrapper classname

--- src/components/cars-list-component/cars-list-component.tsx
+++ src/components/cars-list-component/cars-list-component.tsx
@@ -1,23 +1,45 @@
 import { TCars } from '../../shared/types';
 import { CarListItem } from '../car-list-item';
+import { Pagination } from '../pagination';
 import styles from './cars-list-component.module.scss';
 
 type TCarsList = {
   data?: TCars;
+  currentPage: number;
+  setCurrentPage: (v: number) => void;
+  paginationRange: (string | number)[] | undefined;
+  nextPageClickhandler: () => void;
+  prevPageClickhandler: () => void;
 };
 
-export function CarsListComponent({ data }: TCarsList) {
+export function CarsListComponent({
+  data,
+  currentPage,
+  nextPageClickhandler,
+  paginationRange,
+  prevPageClickhandler,
+  setCurrentPage,
+}: TCarsList) {
   return (
-    <section className={styles.cars_list_container}>
-      {data?.data.map(item => (
-        <CarListItem
-          name={item.name}
-          path={item.thumbnail.path}
-          priceMax={item.priceMax}
-          priceMin={item.priceMin}
-          key={item.id}
-        />
-      ))}
+    <section className={styles.flex_wrapper}>
+      <div className={styles.cars_list_container}>
+        {data?.data.map(item => (
+          <CarListItem
+            name={item.name}
+            path={item.thumbnail.path}
+            priceMax={item.priceMax}
+            priceMin={item.priceMin}
+            key={item.id}
+          />
+        ))}
+      </div>
+      <Pagination
+        currentPage={currentPage}
+        nextPageClickhandler={nextPageClickhandler}
+        paginationRange={paginationRange}
+        prevPageClickhandler={prevPageClickhandler}
+        setCurrentPage={setCurrentPage}
+      />
     </section>
   );
 }
