43a95d311fa5fb10d497b050f81002edef5a8ec8
Denis Dolbilin <kombogame6@gmail.com>
2022-04-05 23:24:14 +0200

    feat: added filter property

--- src/components/filter-cars-form/constants.ts
+++ src/components/filter-cars-form/constants.ts
@@ -2,13 +2,16 @@ export const radioFilterButtonsArr = [
   {
     title: 'Все модели',
     name: 'all',
+    filter: '',
   },
   {
     title: 'Эконом',
     name: 'cheap',
+    filter: 'priceMin[$lt]=200000',
   },
   {
     title: 'Премиум',
-    name: 'premium',
+    name: 'luxury',
+    filter: 'priceMin[$gt]=20000',
   },
 ];
