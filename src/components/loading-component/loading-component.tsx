a53843733a1ee82c77667f671d4d9fd8cd8c3d6d
Denis Dolbilin <kombogame6@gmail.com>
2022-04-07 22:29:49 +0200

    feat: added pagination element skeleton

--- src/components/loading-component/loading-component.tsx
+++ src/components/loading-component/loading-component.tsx
@@ -3,9 +3,12 @@ import styles from './loading-component.module.scss';
 export function LoadingComponent() {
   return (
     <section className={styles.container}>
-      {[1, 2, 3, 4, 5, 6].map(item => (
-        <div className={styles.skeleton_item} key={item}></div>
-      ))}
+      <div className={styles.skeleton_items_block}>
+        {[1, 2, 3, 4, 5, 6].map(item => (
+          <div className={styles.skeleton_item} key={item} />
+        ))}
+      </div>
+      <div className={styles.pagination_item} />
     </section>
   );
 }
