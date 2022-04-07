a4cb9fea17d2e11ca06baf01b0a54864b4c3a81e
Denis Dolbilin <kombogame6@gmail.com>
2022-04-07 22:40:59 +0200

    feat: added buttonTitle array

--- src/pages/order-page/constants.ts
+++ src/pages/order-page/constants.ts
@@ -1,18 +1,25 @@
 export const breadcrumbsArr = [
-    {
-      title: 'Местоположение',
-      linkTo: '/simbirsoft-iteration-one/order',
-    },
-    {
-      title: 'Модель',
-      linkTo: '/simbirsoft-iteration-one/order',
-    },
-    {
-      title: 'Дополнительно',
-      linkTo: '/simbirsoft-iteration-one/order',
-    },
-    {
-      title: 'Итого',
-      linkTo: '/simbirsoft-iteration-one/order',
-    },
-  ];
+  {
+    title: 'Местоположение',
+    linkTo: '/simbirsoft-iteration-one/order',
+  },
+  {
+    title: 'Модель',
+    linkTo: '/simbirsoft-iteration-one/order',
+  },
+  {
+    title: 'Дополнительно',
+    linkTo: '/simbirsoft-iteration-one/order',
+  },
+  {
+    title: 'Итого',
+    linkTo: '/simbirsoft-iteration-one/order',
+  },
+];
+
+export const buttonTitle = [
+  'Выбрать модель',
+  'Дополнительно',
+  'Итого',
+  'Заказать',
+];
