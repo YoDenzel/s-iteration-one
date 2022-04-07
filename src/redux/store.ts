57e386255b12fa8f1255433340883efcc753a302
Denis Dolbilin <kombogame6@gmail.com>
2022-04-07 22:10:02 +0200

    feat: added to store step two slice

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import mapPointsReducer from './map-points-slice/map-points-slice';
 import stepOneOrderFormReducer from './step-one-order-form-slice/step-one-order-form-slice';
+import stepTwoOrderFormReducer from './step-two-order-form-slice/step-two-order-form-slice';
 
 export const store = configureStore({
   reducer: {
     mapPoints: mapPointsReducer,
     stepOneOrderForm: stepOneOrderFormReducer,
+    stepTwoOrderForm: stepTwoOrderFormReducer,
   },
 });
 
