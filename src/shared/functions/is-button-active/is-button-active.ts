710670c07f734733c989073bf56012338b1919bd
Denis Dolbilin <kombogame6@gmail.com>
2022-04-07 22:20:55 +0200

    feat: added case for activeIndex === 1

--- src/shared/functions/is-button-active/is-button-active.ts
+++ src/shared/functions/is-button-active/is-button-active.ts
@@ -1,15 +1,23 @@
-import { useAppSelector } from '../../custom-hooks';
 import { TFirstStepObj } from '../../types';
 
 type TIsButtonActive = {
   activeIndex: number;
   firstStep: TFirstStepObj;
+  secondStep: TFirstStepObj;
 };
 
-export function isButtonActive({ activeIndex, firstStep }: TIsButtonActive) {
+export function isButtonActive({
+  activeIndex,
+  firstStep,
+  secondStep,
+}: TIsButtonActive) {
   switch (activeIndex) {
     case 0: {
       if (firstStep.information.length > 10) return false;
+      else return true;
+    }
+    case 1: {
+      if (secondStep.information) return false;
       else return true;
     }
     default:
