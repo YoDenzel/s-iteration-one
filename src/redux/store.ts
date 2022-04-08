78ed855c41239b617d0f7e99eabd18f6b1ff17f7
Denis Dolbilin <kombogame6@gmail.com>
2022-04-08 20:54:45 +0200

    feat: added slice for status of sidebar

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import mapPointsReducer from './map-points-slice/map-points-slice';
+import sidebarSlice from './sidebar-slice/sidebar-slice';
 
 export const store = configureStore({
   reducer: {
     mapPoints: mapPointsReducer,
+    sidebarSlide: sidebarSlice
   },
 });
 
