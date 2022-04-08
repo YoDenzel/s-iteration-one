10c44056c58a4d24fc17de35f9146c930a3c3cb1
Denis Dolbilin <kombogame6@gmail.com>
2022-04-08 17:54:36 +0200

    refactor: slight changes

--- src/components/order-car-list-component/order-car-list-component.tsx
+++ src/components/order-car-list-component/order-car-list-component.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { setCarName } from '../../redux/step-two-order-form-slice/step-two-order-form-slice';
 import {
   useAppDispatch,
