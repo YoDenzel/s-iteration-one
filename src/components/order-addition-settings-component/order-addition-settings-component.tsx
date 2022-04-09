8b4697c23d228497a8972acc44d0729c488b0908
Denis Dolbilin <kombogame6@gmail.com>
2022-04-09 16:18:09 +0200

    feat: added third order component

---
+++ src/components/order-addition-settings-component/order-addition-settings-component.tsx
@@ -0,0 +1,5 @@
+import styles from './order-addition-settings-component.module.scss';
+
+export function OrderAdditionSettingsComponent() {
+  return <section className={styles.settings_container}>Hello world!</section>;
+}
