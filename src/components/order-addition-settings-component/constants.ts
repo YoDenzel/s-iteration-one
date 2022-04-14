0bb43d39d19670538ba073f433ebc096b21858f1
Denis Dolbilin <kombogame6@gmail.com>
2022-04-14 16:05:32 +0200

    feat: added data for checkbox inputs arr

--- src/components/order-addition-settings-component/constants.ts
+++ src/components/order-addition-settings-component/constants.ts
@@ -1,3 +1,18 @@
-export const filterRadioButtonTitlesArr = ['Любой', 'Голубой', 'Красный'];
+export const filterRadioButtonTitlesArr = ['Любой'];
 
 export const carsRateTitleArr = ['Поминутно, 7₽/мин', 'На сутки, 1999 ₽/сутки'];
+
+export const checkboxArrState = [
+  {
+    title: 'Полный бак, 500р',
+    isActive: false,
+  },
+  {
+    title: 'Детское кресло, 200р',
+    isActive: false,
+  },
+  {
+    title: 'Правый руль, 1600р',
+    isActive: false,
+  },
+];
