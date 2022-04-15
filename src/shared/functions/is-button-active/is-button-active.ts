0c3ed7f0c1bbe5030dbaeea23a33ad15d777fb05
Denis Dolbilin <kombogame6@gmail.com>
2022-04-15 11:24:51 +0200

    feat: added case 3

--- src/shared/functions/is-button-active/is-button-active.ts
+++ src/shared/functions/is-button-active/is-button-active.ts
@@ -1,15 +1,17 @@
-import { TFirstStepObj } from '../../types';
+import { TFirstStepObj, TStepThree } from '../../types';
 
 type TIsButtonActive = {
   activeIndex: number;
   firstStep: TFirstStepObj;
   secondStep: TFirstStepObj;
+  stepThree: TStepThree;
 };
 
 export function isButtonActive({
   activeIndex,
   firstStep,
   secondStep,
+  stepThree,
 }: TIsButtonActive) {
   switch (activeIndex) {
     case 0: {
@@ -18,6 +20,11 @@ export function isButtonActive({
     }
     case 1: {
       if (secondStep.information) return false;
+      else return true;
+    }
+    case 2: {
+      if (stepThree.color && stepThree.rentalDuration && stepThree.rate)
+        return false;
       else return true;
     }
     default:
