1a8a5149c63a96f788e979999a1137cd35545ad5
Denis Dolbilin <kombogame6@gmail.com>
2022-04-15 11:27:53 +0200

    feat: export for additionalServicesCheck functiom

--- src/shared/functions/index.ts
+++ src/shared/functions/index.ts
@@ -2,3 +2,4 @@ export { ChangeStreetView } from './change-street-view';
 export { ChangeCityView } from './change-city-view';
 export { isButtonActive } from './is-button-active';
 export { createPaginationArray } from './create-pagination-array';
+export { additionalServicesCheck } from './additional-services-check';
