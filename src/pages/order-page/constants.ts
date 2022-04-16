7bb6313751594d7d43f9bd2e4838f60703f0b100
Denis Dolbilin <kombogame6@gmail.com>
2022-04-16 12:45:54 +0200

    feat: added function that returning checkbox arr of obj

--- src/pages/order-page/constants.ts
+++ src/pages/order-page/constants.ts
@@ -1,3 +1,5 @@
+import { TStepThree } from '../../shared/types';
+
 export const breadcrumbsArr = [
   {
     title: 'Местоположение',
@@ -23,3 +25,32 @@ export const buttonTitle = [
   'Итого',
   'Заказать',
 ];
+
+export const thirdStepArrObj = (stepThree: TStepThree) => {
+  return [
+    {
+      title: 'Цвет',
+      information: stepThree.color,
+    },
+    {
+      title: 'Длительность аренды',
+      information: stepThree.rentalDuration,
+    },
+    {
+      title: 'Тариф',
+      information: stepThree.rate,
+    },
+    {
+      title: 'Полный бак',
+      information: stepThree.fullTank ? 'Да' : '',
+    },
+    {
+      title: 'Детское кресло',
+      information: stepThree.babyChair ? 'Да' : '',
+    },
+    {
+      title: 'Правый руль',
+      information: stepThree.rightHandDrive ? 'Да' : '',
+    },
+  ];
+};
