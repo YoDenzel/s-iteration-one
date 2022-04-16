2a6cc8b90a570eb2abcc79833595840492377c31
Denis Dolbilin <kombogame6@gmail.com>
2022-04-16 11:48:08 +0200

    feat: export for checkout price rate function

--- src/shared/functions/index.ts
+++ src/shared/functions/index.ts
@@ -3,3 +3,4 @@ export { ChangeCityView } from './change-city-view';
 export { isButtonActive } from './is-button-active';
 export { createPaginationArray } from './create-pagination-array';
 export { additionalServicesCheck } from './additional-services-check';
+export { calculatePriceDependOnRate } from './calculate-price-depend-on-rate';
