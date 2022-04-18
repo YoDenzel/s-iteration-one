b00109fbf92382618dd3f4b14edcb237942ecef3
Denis Dolbilin <kombogame6@gmail.com>
2022-04-18 14:23:30 +0200

    feat: ui layer for popup

---
+++ src/components/order-confirmation-popup/order-confirmation-popup.tsx
@@ -0,0 +1,40 @@
+import { setPopup } from '../../redux/order-confirmation-popup-status-slice/order-confirmation-popup-status-slice';
+import { useAppDispatch } from '../../shared/custom-hooks';
+import styles from './order-confirmation-popup.module.scss';
+
+export function OrderConfirmationPopup() {
+  const dispatch = useAppDispatch();
+  return (
+    <section className={styles.popup_wrapper}>
+      <h1 className={styles.popup_title}>Подтвердить заказ</h1>
+      <form className={styles.buttons_container}>
+        <button
+          type="button"
+          className={styles.agree_button}
+          onClick={() =>
+            dispatch(
+              setPopup({
+                isPopupActive: false,
+              }),
+            )
+          }
+        >
+          Подтвердить
+        </button>
+        <button
+          type="button"
+          className={styles.decline_button}
+          onClick={() =>
+            dispatch(
+              setPopup({
+                isPopupActive: false,
+              }),
+            )
+          }
+        >
+          Вернуться
+        </button>
+      </form>
+    </section>
+  );
+}
