41990e2f8f223c005f7581cc2ddd1b6892e3d338
Denis Dolbilin <kombogame6@gmail.com>
2022-04-21 00:29:00 +0200

    feat: added selector function for getting right data if nedeed

--- src/shared/custom-hooks/use-get-data/use-get-data.ts
+++ src/shared/custom-hooks/use-get-data/use-get-data.ts
@@ -6,13 +6,16 @@ const STALE_TIME = 0;
 type TUseGetData = {
   QUERY_KEY: string;
   url: string;
+  selectorFunction?: any;
 };
 
 export const useGetData = <T>({
   QUERY_KEY,
   url,
+  selectorFunction,
 }: TUseGetData): UseQueryResult<T> => {
   const query = useQuery([QUERY_KEY, url], () => fetchData(url), {
+    select: selectorFunction,
     staleTime: STALE_TIME,
     retry: 0,
   });
