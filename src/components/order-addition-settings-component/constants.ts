1856d71dd8543f8153370559123f47667dce7416
Denis Dolbilin <kombogame6@gmail.com>
2022-04-21 20:30:59 +0200

    refactor: deleted checkboxArrState cause now using it in redux

--- src/components/order-addition-settings-component/constants.ts
+++ src/components/order-addition-settings-component/constants.ts
@@ -1,21 +1,3 @@
 export const filterRadioButtonTitlesArr = ['Любой'];
 
 export const carsRateTitleArr = ['Поминутно, 7₽/мин', 'На сутки, 1999 ₽/сутки'];
-
-export const checkboxArrState = [
-  {
-    title: 'Полный бак, 500р',
-    isActive: false,
-    price: 500,
-  },
-  {
-    title: 'Детское кресло, 200р',
-    isActive: false,
-    price: 200,
-  },
-  {
-    title: 'Правый руль, 1600р',
-    isActive: false,
-    price: 1600,
-  },
-];
