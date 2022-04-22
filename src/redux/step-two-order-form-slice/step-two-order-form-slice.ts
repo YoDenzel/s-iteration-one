e598837c4c781a45af0118d30b69563f5aa28c69
Denis Dolbilin <kombogame6@gmail.com>
2022-04-22 20:55:48 +0200

    feat: added two carFilter and carFilterButtonTitle so its not reseted after activeComponent changes

--- src/redux/step-two-order-form-slice/step-two-order-form-slice.ts
+++ src/redux/step-two-order-form-slice/step-two-order-form-slice.ts
@@ -1,23 +1,32 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { TCarsData } from '../../shared/types';
 import { RootState } from '../store';
+import { TCarFilter } from './types';
 
 export const stepTwoOrderForm = createSlice({
   name: 'stepTwoOrderForm',
   initialState: {
     car: {} as TCarsData,
+    carFilter: '',
+    carFilterButtonTitle: '',
   },
   reducers: {
     setCarItem: (state, action: PayloadAction<{ car: TCarsData }>) => {
       state.car = action.payload.car;
     },
-    clearStepTwoStore: (state) => {
-      state.car = {} as TCarsData
-    }
+    clearStepTwoStore: state => {
+      state.car = {} as TCarsData;
+      state.carFilter = '';
+    },
+    setCarFilter: (state, action: PayloadAction<TCarFilter>) => {
+      state.carFilter = action.payload.carFilter;
+      state.carFilterButtonTitle = action.payload.carFilterButtonTitle;
+    },
   },
 });
 
-export const { setCarItem, clearStepTwoStore } = stepTwoOrderForm.actions;
+export const { setCarItem, clearStepTwoStore, setCarFilter } =
+  stepTwoOrderForm.actions;
 
 export default stepTwoOrderForm.reducer;
 
