6414eb551a1e15281a17b598bba141ffc80faa75
Denis Dolbilin <kombogame6@gmail.com>
2022-04-23 21:38:40 +0200

    feat: function for transform stepThreeObject properties in array

---
+++ src/shared/functions/step-three-obj-into-array/step-three-obj-into-array.ts
@@ -0,0 +1,30 @@
+import { TStepThree } from '../../types';
+
+export const stepThreeObjIntoArray = (stepThree: TStepThree) => {
+  return [
+    {
+      title: 'Цвет',
+      information: stepThree.color,
+    },
+    {
+      title: 'Длительность аренды',
+      information: stepThree.rentalDuration,
+    },
+    {
+      title: 'Тариф',
+      information: stepThree.rate,
+    },
+    {
+      title: 'Полный бак',
+      information: stepThree.fullTank ? 'Да' : '',
+    },
+    {
+      title: 'Детское кресло',
+      information: stepThree.babyChair ? 'Да' : '',
+    },
+    {
+      title: 'Правый руль',
+      information: stepThree.rightHandDrive ? 'Да' : '',
+    },
+  ];
+};
