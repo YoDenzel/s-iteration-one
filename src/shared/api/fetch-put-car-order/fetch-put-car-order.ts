a503669ecb351f64c1eb3e31e377a6000088431a
Denis Dolbilin <kombogame6@gmail.com>
2022-04-25 12:23:32 +0200

    refactor: reusing request options

--- src/shared/api/fetch-put-car-order/fetch-put-car-order.ts
+++ src/shared/api/fetch-put-car-order/fetch-put-car-order.ts
@@ -1,17 +1,12 @@
 import { TPutCarOrderStatus } from '../../types';
+import { requestOptions } from '../constants';
 
 export const fetchPutCarOrder = async ({
   orderId,
   orderStatusId,
 }: TPutCarOrderStatus) => {
-  const requestOptions = {
-    method: 'PUT',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-      Authorization: 'Bearer 52efbe08228671240494f537f2486bc109a637b4',
-      'X-Api-Factory-Application-Id': `${process.env.REACT_APP_API_KEY}`,
-    },
+  const request = {
+    ...requestOptions,
     body: JSON.stringify({
       orderStatusId: {
         id: orderStatusId?.id,
@@ -22,7 +17,7 @@ export const fetchPutCarOrder = async ({
 
   const response = await fetch(
     `https://api-factory.simbirsoft1.com/api/db/order/${orderId}`,
-    requestOptions,
+    request,
   );
   if (!response.ok) throw new Error(response.statusText);
   const data = response.json();
